026c1efb8d64dc2851b7a623d4d07018f7bc2af9
mbaile66 <michael.allan.bailey@gmail.com>
2014-06-06 14:13:56 -0400

    Update subtractionStimPlugin.js

--- scripts/plugins/subtractionStimPlugin.js
+++ scripts/plugins/subtractionStimPlugin.js
@@ -9,40 +9,35 @@ $('body').append($('<div>', {
                     id: 'jspsych-single-stim-stimulus'
                 }));
 
-function createStimulus() { 
-
-	var newCanvas = $('body').append($('<canvas/>',{'id': 'Canvas', 'class':'radHuh'})
-		.width(130)
-		.height(50));
-	var min = 1;
-	var max = 20;
-	var A = Math.floor(Math.random() * (max - min + 1)) + min;
-	var B = Math.floor(Math.random() * (max - min + 1)) + min;
-	var C = Math.floor(Math.random() * (max - min + 1)) + min;
-	var randomNumber1 = Math.random();
-	var randomNumber2 = Math.random();
-	var P1 = Math.floor(randomNumber1 * 11);
-	var P2 = Math.floor(randomNumber2 * 11);
-	//var solVal = (A - B - C);
-	var x = document.getElementById("Canvas");
-	var ctx = x.getContext("2d");
-	ctx.font = "12px Arial";
-	ctx.font = "14px Arial";
-	ctx.fillText("-",35,40);
-	ctx.fillText("-",75,40);
-	while ((A - B - C) < 0)
-	{ 
-		//A = Math.floor(Math.random() * (max - min + 1)) + min;
-		B = Math.floor(Math.random() * (max - min + 1)) + min;
-		C = Math.floor(Math.random() * (max - min + 1)) + min;
-	}
-	ctx.fillText(A,10,40);
-	ctx.fillText(B,50,40);
-	ctx.fillText(C,90,40);
-	while (P1 === P2)
-	{
-		P2 = Math.floor(randomNumber2 * 11);
-	}
-	ctx.fillText(P1,30,25);
-	ctx.fillText(P2,70,25);
+function createStimulus() 
+{
+var min = 1;
+var max = 20;
+var A = Math.floor(Math.random() * (max - min + 1)) + min;
+var B = Math.floor(Math.random() * (max - min + 1)) + min;
+var C = Math.floor(Math.random() * (max - min + 1)) + min;
+var randomNumber1 = Math.random();
+var randomNumber2 = Math.random();
+var P1 = Math.floor(randomNumber1 * 11);
+var P2 = Math.floor(randomNumber2 * 11);
+var paper = Raphael(10, 50, 320, 200);
+var t = paper.text(50, 50, A);
+var u = paper.text(65, 50, "-");
+var v = paper.text(80, 50, B);
+var w = paper.text(95, 50, "-");
+var x = paper.text(110, 50, C);
+var y = paper.text(65, 40, P1);
+var z = paper.text(95, 40, P2);
+while ((A - B - C) < 0)
+{ 
+	//A = Math.floor(Math.random() * (max - min + 1)) + min;
+	B = Math.floor(Math.random() * (max - min + 1)) + min;
+	C = Math.floor(Math.random() * (max - min + 1)) + min;
+}
+//document.write(t,u,v);
+while (P1 === P2)
+{
+	P2 = Math.floor(randomNumber2 * 11);
+}
+//document.write(t);
 }
