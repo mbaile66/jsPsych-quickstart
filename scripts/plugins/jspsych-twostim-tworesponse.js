5968c7843b4a5c2d681823f0f4d88be51a5f4ebe
Josh de Leeuw <josh.deleeuw@gmail.com>
2014-06-12 21:11:27 -0400

    temp remove normalizeTrialVariables

--- scripts/plugins/jspsych-twostim-tworesponse.js
+++ scripts/plugins/jspsych-twostim-tworesponse.js
@@ -55,7 +55,9 @@
             // if any trial variables are functions
             // this evaluates the function and replaces
             // it with the output of the function
-            trial = jsPsych.normalizeTrialVariables(trial);
+            
+            // put this back in after updating to jsPsych v2.4
+            //trial = jsPsych.normalizeTrialVariables(trial);
             
             
             switch (part) {
