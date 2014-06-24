9629016ddea8ffa8120fe187134f0dfc946ffec5
mbaile66 <bailemia@indiana.edu>
2014-06-24 14:34:33 -0400

    Update subtractionStimPlugin.js

--- scripts/plugins/subtractionStimPlugin.js
+++ scripts/plugins/subtractionStimPlugin.js
@@ -1,62 +1,45 @@
-function createStimulus(stim_type)
-{
-	// create a div to hold stimulus
+function createStimulus (numbers, showProbe){
 	$('body').append($('<div id="stimulus"></div>'));
-
-	// create the raphael object
-	var width = 320;
-	var height = 200;
-	var paper = Raphael("stimulus", width, height);
-
-	var min = 1;
-	var max = 20;
-	var A = Math.floor(Math.random() * (max - min + 1)) + min;
-	var B = Math.floor(Math.random() * (max - min + 1)) + min;
-	var C = Math.floor(Math.random() * (max - min + 1)) + min;
-
-	var P1 = Math.floor(Math.random()  * 11);
-	var P2 = Math.floor(Math.random()  * 11);
 	
-	while ((A - B - C) < 0)
-	{ 
-		A = Math.floor(Math.random() * (max - min + 1)) + min;
-		B = Math.floor(Math.random() * (max - min + 1)) + min;
-		C = Math.floor(Math.random() * (max - min + 1)) + min;
-	}
-		var t = paper.text(50, 50, A);
-		var u = paper.text(65, 50, "-");
-		var v = paper.text(80, 50, B);
-		var w = paper.text(95, 50, "-");
-		var x = paper.text(110, 50, C);
+	var width = 600;
+	var height = 400;
+	var paper = Raphael("stimulus", width, height);
+	paper.text(50,50,numbers[0]);
+	paper.text(65, 50, "-");
+	paper.text(80, 50, numbers[2]);
+	paper.text(95, 50, "-");
+	paper.text(110, 50, numbers[4]);
+	
+	if (showProbe == 1){
+		paper.text(65, 40, numbers[1]);
+	} else if(showProbe == 2){
+		paper.text(95, 40, numbers[3]);
+	} else if(showProbe == 3){
+		paper.text(65, 40, numbers[3]);
+	} else if(showProbe == 4){
+		paper.text(95, 40, numbers[1]);
+    } else if(showProbe == 5){
 		
-/*if (stim_type == 1) //2 probes, 0ms delay
-	{
-	while (P1 === P2)
-		{
-		P2 = Math.floor(Math.random() * 11);
+			//generate a random number named randProbeA unique to row and +-2 of [1]
+			var randProbeA = [0,1,2,3,4,5,6,7,8,9]
+			//remove all items from array that are already in the row and whose distance from [1] > 2
+			_.without((randProbeA),numbers[0],numbers[1],numbers[2],numbers[3],numbers[4],(numbers[1]+3),(numbers[1]-3),(numbers[1]+4),(numbers[1]-4),(numbers[1]+5),(numbers[1]-5),(numbers[1]+6),(numbers[1]-6),(numbers[1]+7),(numbers[1]-7),(numbers[1]+8),(numbers[1]-8),(numbers[1]+9),(numbers[1]-9));
+			//update randProbeA to become a randomly selected number from the remaining array 
+			randProbeA = randProbeA[Math.floor(Math.random()*items.length)];
+			paper.text(65, 40, randProbeA);			
+	
+	} else if(showProbe == 6){
+			
+			var randProbeB = [0,1,2,3,4,5,6,7,8,9]
+			
+			_.without((randProbeB),numbers[0],numbers[1],numbers[2],numbers[3],numbers[4],(numbers[3]+3),(numbers[3]-3),(numbers[3]+4),(numbers[3]-4),(numbers[3]+5),(numbers[3]-5),(numbers[3]+6),(numbers[3]-6),(numbers[3]+7),(numbers[3]-7),(numbers[3]+8),(numbers[3]-8),(numbers[3]+9),(numbers[3]-9));
+			
+			randProbeA = randProbeA[Math.floor(Math.random()*items.length)];
+			
+			paper.text(65, 40, randProbeA);
 		}
-		var y = paper.text(65, 40, P1);
-		var z = paper.text(95, 40, P2);
-	}
-else if (stim_type) = 2 //first probe, 0ms delay
-	{	
-	while (P1 === P2)
-		{
-		P2 = Math.floor(Math.random() * 11);
-		}
-		var y = paper.text(65, 40, P1);
-	}	
-else if (stim_type) = 3//second probe, 0ms delay
-	{	
-	while (P1 === P2)
-		{
-		P2 = Math.floor(Math.random() * 11);
-		}
-		var z = paper.text(95, 40, P2);
-	}	
-else //no probe, 0ms delay
-*/	
+
 	var html = $('#stimulus').html();
 	$('#stimulus').remove();
 	return html;
-}
+} 
