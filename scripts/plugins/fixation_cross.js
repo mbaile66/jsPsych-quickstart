9d9508d372f7eafe9daf706d0ed57c3a4f183a8f
mbaile66 <bailemia@indiana.edu>
2014-06-30 17:50:25 -0400

    reordered vars, changed canvas size

--- scripts/plugins/fixation_cross.js
+++ scripts/plugins/fixation_cross.js
@@ -1,9 +1,9 @@
 function createFixation (){
 	$('body').append($('<div id="stimulus"></div>'));
 	
-	var paper = Raphael("stimulus", width, height);
 	var width = 800;
-	var height = 600;
+	var height = 800;
+	var paper = Raphael("stimulus", width, height);
 	var xPosition = 250;
 		
 	paper.text(xPosition+175, 350, "Hello").attr({'font-size':48});	
