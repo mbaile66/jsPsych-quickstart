3466af0e945564cbda3da4526f6dbf80bc80c0f3
mbaile66 <bailemia@indiana.edu>
2014-07-02 15:32:24 -0400

    move questions on bottom of screen

--- scripts/plugins/subtractionStimPlugin.js
+++ scripts/plugins/subtractionStimPlugin.js
@@ -11,16 +11,16 @@ function createStimulus (numbers, showProbe, showFeedback, answerCorrect, probeC
 	paper.text(xPosition+125, 150, numbers[2]).attr({'font-size':48});
 	paper.text(xPosition+175, 150, "-").attr({'font-size':48});
 	paper.text(xPosition+225, 150, numbers[4]).attr({'font-size':48});
-	paper.text(xPosition+75, yPosition+100, "Answer  =").attr({'font-size':30});
+	paper.text(xPosition+125, yPosition+100, "Answer  =").attr({'font-size':30});
 	
 	if (numbers[6] == 4){
-		paper.text(xPosition+75, yPosition+150, "Is 4  present?").attr({'font-size':30});
+		paper.text(xPosition+25, yPosition+150, "4  present?").attr({'font-size':30});
 	}
 	else if (numbers[6] == 5){
-		paper.text(xPosition+75, yPosition+150, "Is 5  present?").attr({'font-size':30});
+		paper.text(xPosition+25, yPosition+150, "5  present?").attr({'font-size':30});
 	}
 	else {
-		paper.text(xPosition+75, yPosition+150, "Is 6  present?").attr({'font-size':30});
+		paper.text(xPosition+25, yPosition+150, "6  present?").attr({'font-size':30});
 	}
 	if (showProbe == 1){
 		paper.text(xPosition+75, 125, numbers[1]).attr({'font-size':24});
