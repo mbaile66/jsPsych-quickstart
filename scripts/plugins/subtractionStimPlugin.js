5e404bbf7519578e93f9497a084dc5a8065db7ce
mbaile66 <bailemia@indiana.edu>
2014-07-09 13:40:15 -0400

    change feedback display

--- scripts/plugins/subtractionStimPlugin.js
+++ scripts/plugins/subtractionStimPlugin.js
@@ -11,27 +11,31 @@ function createStimulus (numbers, showProbe, showFeedback, answerCorrect, probeC
 	paper.text(xPosition+125, 150, numbers[2]).attr({'font-size':48});
 	paper.text(xPosition+175, 150, "-").attr({'font-size':48});
 	paper.text(xPosition+225, 150, numbers[4]).attr({'font-size':48});
-	paper.text(xPosition+175, yPosition+100, "Answer  =").attr({'font-size':30});
+	
 
-/*	
-	if (numbers[6] == 4){
-		paper.text(xPosition+25, yPosition+150, "4  present? (y/n)").attr({'font-size':30});
-	}
-	else if (numbers[6] == 5){
-		paper.text(xPosition+25, yPosition+150, "5  present? (y/n)").attr({'font-size':30});
-	}
-	else {
-		paper.text(xPosition+25, yPosition+150, "6  present? (y/n)").attr({'font-size':30});
-	}
+	
+
 	if (showProbe == 1){
 		paper.text(xPosition+75, 125, numbers[1]).attr({'font-size':24});
 	} else if(showProbe == 2){
 		paper.text(xPosition+175, 125, numbers[3]).attr({'font-size':24});
 	}
 
-*/
 	
 	if(showFeedback == true){
+		
+		paper.text(xPosition+175, yPosition+100, "Answer  =").attr({'font-size':30});
+		
+		if (numbers[6] == 4){
+		paper.text(xPosition+25, yPosition+150, "4  present?").attr({'font-size':30});
+		}
+		else if (numbers[6] == 5){
+		paper.text(xPosition+25, yPosition+150, "5  present?").attr({'font-size':30});
+		}
+		else {
+		paper.text(xPosition+25, yPosition+150, "6  present?").attr({'font-size':30});
+		}
+		
 		if(answerCorrect == true){
 			paper.image("img/checkmark.gif", xPosition+70, yPosition+80, 29, 35);
 			} else {
